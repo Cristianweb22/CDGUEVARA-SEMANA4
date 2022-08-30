70626d29892aab3a77754799964effaf573bd5cb
Cristianweb22 <110673277+Cristianweb22@users.noreply.github.com>
2022-08-30 15:33:56 -0500

    Add files via upload
    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+const tasks = [
+    {done: false, text: "Crear HTML" },
+    {done: true, text: "Linkear CSS" },
+    {done: false, text: "Linkear JS" },
+    {done: false, text: "Maquetar" },
+    {done: false, text: "Crear array de elementos en JS" },
+    {done: false, text: "Definir clases" },
+    {done: false, text: "añadir ONCLICK" },
+    {done: false, text: "Mandar array a lista de AllList" },
+    {done: false, text: "enviar elementos chequeados a finList" },
+    {done: true, text: "enviar elementos no chequeados a pendList" }
+];
+
+const allTaskList = document.getElementById("all-task");
+const pendTaskList = document.getElementById("pend-task");
+const finTaskList = document.getElementById("fin-task");
+
+
+function createTasks(){
+    allTaskList.innerHTML="";
+    pendTaskList.innerHTML="";
+    finTaskList.innerHTML="";
+
+    tasks.forEach((taskItem) => {
+        const elem = document.createElement("li");
+        elem.textContent = taskItem.text;
+        elem.className = taskItem.done ? "checked":"unchecked";
+        //console.log(taskItem.done);
+        elem.addEventListener("click",() =>toggleTask(taskItem));
+        
+        const cloneElem = elem.cloneNode(true);
+        cloneElem.addEventListener("click",()=>toggleTask(taskItem));
+
+        allTaskList.append(elem);
+        
+        if(taskItem.done){
+            finTaskList.append(cloneElem);
+            //console.log(taskItem)
+        }else{
+            pendTaskList.append(cloneElem);
+        }
+    });
+}
+createTasks();
+
+function toggleTask(taskItem){
+    taskItem.done=!taskItem.done;
+    createTasks();
+}
+
+
+//como putas uso git?
